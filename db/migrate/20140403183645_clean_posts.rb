8f92d1a1c3d3bf6851405e28183a1dc586125e52
Jason Rush <diminish7@gmail.com>
2014-04-03 13:00:04 -0600

    Added migration to clean up blogger-formatted post HTML

---
+++ db/migrate/20140403183645_clean_posts.rb
@@ -0,0 +1,38 @@
+class CleanPosts < ActiveRecord::Migration
+  def up
+    Post.all.each do |post|
+      doc = Nokogiri::HTML::DocumentFragment.parse(post.body)
+      clean_node(doc)
+      post.update_attribute(:body, doc.to_html)
+    end
+  end
+
+  def down
+    # NOOP
+  end
+
+private
+  def clean_node(node)
+    node.children.each do |child|
+      case child
+      when Nokogiri::XML::Text
+        # Get rid of empty text nodes
+        child.remove if child.text.blank?
+      when Nokogiri::XML::Element
+        # Get rid of any built in styles
+        child.attributes['style'].try(:remove)
+        if child.name == "div"
+          # Recurse on divs
+          clean_node(child)
+          if child.children.blank?
+            # Remove empty elements
+            child.remove
+          elsif child.children.none? { |c| c.name == "p" }
+            # As long as this isn't wrapping other paragraphs, make it a paragraph
+            child.name = "p"
+          end
+        end
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112191643) do
+ActiveRecord::Schema.define(:version => 20140403183645) do
 
   create_table "blogs", :force => true do |t|
     t.string   "name"
