79673d4c61ce9c5b9f9b0bdbb7890e345eb83279
Jason Rush <diminish7@gmail.com>
2014-11-24 12:27:24 -0700

    Changed secret token and moved it out of git to gitignored file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jasonrush::Application.config.secret_token = '5e6a1511576c3645edacd6db94f83ef9490ad58d8c9da85df45acb0863fc37baf6b61831b59a3bafd5f00df9253b40d968464f26acc6db9f8d15f8a193d53172'
+Jasonrush::Application.config.secret_token = File.open(File.join(Rails.root, 'config/secret_token'), 'r') { |f| f.gets }
